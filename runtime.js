bc561462e24b2afa262c7104e7c3b239e7e1204b
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-09-24 02:46:08 +0300

    Add plugin

---
+++ runtime.js
@@ -0,0 +1,98 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.pakohuone = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.pakohuone.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		
+		// any other properties you need, e.g...
+		// this.myValue = 0;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+		// note the object is sealed after this call; ensure any properties you'll ever need are set on the object
+		// e.g...
+		// this.myValue = 0;
+	};
+	
+	// only called if a layout object - draw to a canvas 2D context
+	instanceProto.draw = function(ctx)
+	{
+	};
+	
+	// only called if a layout object in WebGL mode - draw to the WebGL context
+	// 'glw' is not a WebGL context, it's a wrapper - you can find its methods in GLWrap.js in the install
+	// directory or just copy what other plugins do.
+	instanceProto.drawGL = function (glw)
+	{
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	
+	pluginProto.cnds = new Cnds();
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	
+	Acts.prototype.UnlockEvent = function ()
+	{
+		const message = {
+			pakohuone: true,
+			type: 'Event',
+			data: {
+				name: 'Unlock',
+				parameter: this.runtime.projectName
+			}
+		};
+	
+		parent.postMessage(JSON.stringify(message));
+	};
+	
+	pluginProto.acts = new Acts();
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	
+	pluginProto.exps = new Exps();
+
+}());
